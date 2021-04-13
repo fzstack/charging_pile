8e0f89dfda77eb5c1eb082ebd927285aed2171fe
imgcre <imgcre@outlook.com>
2021-04-13 23:18:33 +0800

    [特性]进度条

---
+++ ui/progress_widget.cxx
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2006-2020, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2021-04-13     imgcr       the first version
+ */
+
+#include "progress_widget.hxx"
+#include <utilities/math.hxx>
+#include <cmath>
+
+void ProgressWidget::show() {
+    foreColor.setTarget(kForeColor);
+    backColor.setTarget(kBackColor);
+}
+
+void ProgressWidget::hide() {
+    foreColor.setTarget(Colors::Argb::kTransparent);
+    backColor.setTarget(Colors::Argb::kTransparent);
+}
+
+void ProgressWidget::setProgress(int value) {
+    progress.setTarget(value);
+}
+
+void ProgressWidget::onDraw(std::shared_ptr<Graphics> graphics) {
+    graphics->fillRect(backColor.get(), 0, 0, getWidth(), getHeight());
+    //绘制实色区
+    auto len = (float)getWidth() * progress.get() / 100;
+    graphics->addRect(foreColor.get(), 0, 0, len, getHeight());
+    auto rem = fmodf(len, 1.f);
+    auto lineColor = foreColor.get();
+    lineColor.a *= rem;
+    graphics->addVLine(lineColor, len, 0, getHeight() - 1);
+}
+
+void ProgressWidget::onTick() {
+    foreColor.update();
+    backColor.update();
+    progress.update();
+    invalid();
+}
+
+float ProgressWidget::PVel::ease(float x) {
+    return 1 - (1 - x) * (1 - x);
+}
+
+int ProgressWidget::PVel::intepl(const int& originVal, const int& targetVal, float pos) {
+    return between(originVal, targetVal, pos);
+}
+
+const Colors::Argb
+    ProgressWidget::kForeColor = {90, 100, 0, 200},
+    ProgressWidget::kBackColor = {200, 0, 0, 0};
---
+++ ui/progress_widget.hxx
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2006-2020, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2021-04-13     imgcr       the first version
+ */
+#pragma once
+#include "widget.hxx"
+#include "color_vel.hxx"
+
+class ProgressWidget: public CSizeWidget<5, 2> {
+    using p_t = CSizeWidget<5, 2>;
+    using p_t::p_t;
+public:
+    void show();
+    void hide();
+    void setProgress(int value);
+protected:
+    virtual void onDraw(std::shared_ptr<Graphics> graphics) override;
+    virtual void onTick() override;
+private:
+    class PVel: public Vel<int> {
+        using Vel<int>::Vel;
+    public:
+        virtual float ease(float x) override;
+        virtual int intepl(const int& originVal, const int& targetVal, float pos) override;
+    };
+
+    ColorVel foreColor = {Colors::Argb::kTransparent, vel_step(500)}, backColor = {Colors::Argb::kTransparent, vel_step(2000)};
+    PVel progress = {0, vel_step(1000)};
+    static const Colors::Argb kForeColor, kBackColor;
+};
+
+#include <utilities/singleton.hxx>
+#include "core.hxx"
+#include "ws_screen.hxx"
+namespace Preset {
+class ProgressWidget: public Singleton<ProgressWidget>, public ::ProgressWidget {
+    friend singleton_t;
+    ProgressWidget(): ::ProgressWidget(0, 0, zIndex) {
+        auto holder = std::shared_ptr<::ProgressWidget>(this, [](auto){});
+        Core::get()->add(shared_from_this());
+    }
+
+    static const int zIndex;
+};
+}
